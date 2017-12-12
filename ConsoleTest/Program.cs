c7cf9699192a285e94788a008e6f8d190391dc8c
Shaun Anderson <shaun@wemakeapps.net>
2017-12-12 15:45:45 +1100

    Created Core 2.0 Console App

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime;
+using System.Runtime.InteropServices;
+using Plugin.GoogleAnalytics;
+
+namespace TestConsoleApplication
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            GoogleAnalytics.Current.Config.TrackingId = "UA-11111111-1";
+            GoogleAnalytics.Current.Config.AppId = "AppID";
+            GoogleAnalytics.Current.Config.AppName = "TEST";
+            GoogleAnalytics.Current.Config.AppInstallerId = Guid.NewGuid().ToString();
+            GoogleAnalytics.Current.Tracker.UserAgentOverride = "dfsfsdf";
+
+            //  GoogleAnalytics.Current.Config.Debug = true;
+            GoogleAnalytics.Current.InitTracker();
+
+            try
+            {
+                GoogleAnalytics.Current.Tracker.SendView("");
+                GoogleAnalytics.Current.Tracker.SendView("MainPage");
+            }
+            catch (Exception ex)
+            {
+                int a = 5;
+            }
+
+            Console.ReadLine();
+        }
+
+        private static void Tets2()
+        {
+            //GA.Config.TrackingId = "UA-1111111";
+
+            //GA.Tracker.SendEvent("sdfdsf");
+        }
+    }
+}
